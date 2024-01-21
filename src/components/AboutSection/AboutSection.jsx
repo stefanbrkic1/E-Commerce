298653fcfe0c398ccd6390ea773283ee53c3de47
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-01-21 18:23:42 +0100

    Added AboutUs Section

---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,33 @@
+import './about-section.css';
+
+function AboutSection() {
+  return (
+    <section className="about-section-container">
+      <div className="about-section">
+        <div className="about-section-left">
+          <div className="about-logo"></div>
+        </div>
+        <div className="about-section-right">
+          <h3 className="about-header">ABOUT US</h3>
+          <p className="about-para">
+            At Shoping Commerce, we&apos;re not just an online store, we&apos;re
+            a destination for those who seek quality, variety, and a touch of
+            uniqueness in every purchase. Our philosophy revolves around the
+            belief that shopping should be an experience that reflects
+            individuality and personal style. As a brand, we don&apos;t just
+            offer products; we curate a collection that caters to diverse tastes
+            and preferences. From fashion-forward apparel to trendsetting
+            accessories, home essentials to tech gadgets, we&apos;ve got it all.
+            Our mission is simple, to provide you with a one-stop-shop where you
+            can discover, explore, and confidently say, &apos;This is me.&apos;
+            Our commitment to quality and customer satisfaction is unwavering,
+            ensuring that every product you choose is not just a purchase but a
+            statement of your individuality.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutSection;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar/Navbar';
 import HeroSection from '../components/HeroSection/HeroSection';
 import PopularItems from '../components/PopularItems/PopularItems';
+import AboutSection from '../components/AboutSection/AboutSection';
 import PropTypes from 'prop-types';
 
 function HomePage({ productsData, loading, error }) {
@@ -13,6 +14,7 @@ function HomePage({ productsData, loading, error }) {
         loading={loading}
         error={error}
       />
+      <AboutSection />
     </>
   );
 }
