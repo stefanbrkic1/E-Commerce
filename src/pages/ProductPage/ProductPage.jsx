ba7964d80ce8099c912d857f09a90d64e440614c
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-01-26 18:35:36 +0100

    Added Product Page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -23,7 +23,64 @@ function ProductPage() {
   return (
     <>
       <Navbar />
-      <div className="product-page-container"></div>
+      <div className="product-page-container">
+        <div className="product-container">
+          <div className="product-images">
+            {product && (
+              <>
+                <div className="product-secondary-images">
+                  <div
+                    className="product-secondary-image"
+                    style={{ backgroundImage: `url(${product.images[0]})` }}
+                  ></div>
+                  <div
+                    className="product-secondary-image"
+                    style={{ backgroundImage: `url(${product.images[1]})` }}
+                  ></div>
+                  <div
+                    className="product-secondary-image"
+                    style={{ backgroundImage: `url(${product.images[2]})` }}
+                  ></div>
+                </div>
+                <div
+                  className="product-container-img"
+                  style={{ backgroundImage: `url(${product.images[0]})` }}
+                ></div>
+              </>
+            )}
+          </div>
+
+          <div className="product-container-info">
+            {product && (
+              <>
+                <div className="category-displayer">
+                  {product.category.name}
+                </div>
+                <div className="title-of-product">{product.title}</div>
+                <div className="product-info-description">
+                  {product.description}
+                </div>
+              </>
+            )}
+
+            <div className="product-info-rating">
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+            </div>
+            <div className="product-info-price-heading">Price</div>
+            {product && (
+              <div className="product-info-price">{product.price}$</div>
+            )}
+
+            <button type="button" className="add-to-cart-btn">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
