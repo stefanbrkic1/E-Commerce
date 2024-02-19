8751f6ac7b769d176427935c5351edbae4546809
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-02-19 18:28:23 +0100

    Refactored React Router to use newer syntax

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import ProductsPage from '../pages/ProductsPage/ProductsPage';
 import ProductPage from '../pages/ProductPage/ProductPage';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  { path: '/products/:category', element: <ProductsPage /> },
+  { path: '/product/:productId', element: <ProductPage /> },
+]);
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/products/:category" element={<ProductsPage />} />
-        <Route path="/product/:productId" element={<ProductPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default Router;
